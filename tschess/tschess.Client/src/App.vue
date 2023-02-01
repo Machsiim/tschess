c0fe3fd2766f309e9c72074e43580e63d1183d31
Say My Name <mohamadaldulemi@gmail.com>
2023-02-01 10:36:38 +0100

    Commit 20230201

--- tschess/tschess.Client/src/App.vue
+++ tschess/tschess.Client/src/App.vue
@@ -4,12 +4,33 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+  
+      </ul>
+    </div>
+  </div>
+</nav>
 
     
   </header>
 
-  <RouterView />
+  <RouterView/>
 </template>
 
 <style >
