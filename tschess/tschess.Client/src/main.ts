519170b795c19baafb9a132d38a5cd2c7dd22057
Say My Name <mohamadaldulemi@gmail.com>
2023-02-20 10:02:32 +0100

    Homepage

--- tschess/tschess.Client/src/main.ts
+++ tschess/tschess.Client/src/main.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
-import process from 'node:process'
+//import axios from "axios";
+//import process from 'node:process'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -8,7 +8,7 @@ import App from './App.vue'
 import router from './router'
 
 
-axios.defaults.baseURL = process.env.NODE_ENV == 'production' ? "/api" : "https://localhost:5001/api";
+//axios.defaults.baseURL = process.env.NODE_ENV == 'production' ? "/api" : "https://localhost:5001/api";
 
 const app = createApp(App)
 
--- tschess/tschess.Client/src/views/HomeView.vue
+++ tschess/tschess.Client/src/views/HomeView.vue
@@ -4,6 +4,59 @@
 
 <template>
   <main>
-    Dies ist die Homepage 
+    <!DOCTYPE html>
+<html>
+<head>
+	<title>Schach Homepage</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+</head>
+<body>
+	<header>
+		<h1>Schach Homepage</h1>
+	</header>
+	
+	<section>
+		<h2>Willkommen auf unserer Schach-Homepage</h2>
+		<p>Hier finden Sie Informationen über das Schachspiel, die Schachregeln, Schachstrategien, Schachnotation und vieles mehr.</p>
+		<p>Wir bieten auch eine Plattform zum Spielen von Schach online gegen andere Spieler, sowie Schachpuzzles und Schachvideos an.</p>
+	</section>
+	
+
+</body>
+<footer>
+		<p>&copy; 2023 Schach Homepage</p>
+	</footer>
+</html>
+
   </main>
 </template>
+
+<style>
+		body {
+			margin: 0;
+			padding: 0;
+			font-family: Arial, sans-serif;
+			background-color: #f2f2f2;
+		}
+		
+		section {
+			float: left;
+			width: 70%;
+			padding: 20px;
+		}
+		aside {
+			float: left;
+			width: 30%;
+			padding: 20px;
+		}
+		footer {
+			background-color: #222;
+			color: #fff;
+			padding: 20px;
+			text-align: center;
+			font-size: 0.8em;
+			clear: both;
+		}
+	</style>
