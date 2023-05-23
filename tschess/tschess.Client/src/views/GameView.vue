1d9322541297640cb0c15f63d4fbf0c3313ca891
Grosserzbischof <eric.a.fischer@icloud.com>
2023-05-23 12:50:33 +0200

    gameView überarbeitet

--- tschess/tschess.Client/src/views/GameView.vue
+++ tschess/tschess.Client/src/views/GameView.vue
@@ -4,29 +4,35 @@ import signalRService from "../services/SignalRService.js";
 import { TheChessboard } from "vue3-chessboard";
 import "vue3-chessboard/style.css";
 import { ref } from "vue";
+import { reactive } from "vue";
 </script>
 
 <template>
-  <TheChessboard
-    :board-config="boardConfig"
-    @board-created="(api) => (boardAPI = api)"
-    @checkmate="gameEnd"
-    @move="handleMove"
-  />
+  <center>
+    <TheChessboard
+      :board-config="boardConfig"
+      :player-color="color"
+      @board-created="(api) => (boardAPI = api)"
+      @checkmate="gameEnd"
+      @move="handleMove"
+    />
+  </center>
 </template>
 
 <script>
-const color = 2;
-const boardConfig = {
+const playerColor = "white";
+const pneis = "yoschof";
+boardConfig = reactive({
   moveable: {
     free: false,
   },
-};
+});
 const boardAPI = ref();
 export default {
   data() {
     return {
       gameState: "",
+      pneis: "yoschof",
     };
   },
   mounted() {
@@ -51,18 +57,20 @@ export default {
     },
 
     setGameState(s) {
-      boardAPI.value.setPosition(s[0]);
-      if (this.$store.state.infos.username == s[1]) {
-        this.color = 0;
-      } else this.color = 1;
-
-      if (this.color == 1) {
-        boardConfig.value = {
-          ...boardConfig.value,
-          movable: { color: "black" },
-          orientation: "black",
-        };
+      if (this.pneis == "yoschof") {
+        if (this.$store.state.infos.username == s[1]) {
+          this.pneis = "white";
+        } else {
+          this.pneis = "black";
+          //boardConfig.value = { ...boardConfig.value, orientation: "black" };
+          //boardConfig.value.orientation = "black";
+        }
       }
+
+      console.log(this.pneis);
+      boardAPI.value.setPosition(s[0]);
+      console.log(s[0]);
+      return this.pneis;
     },
   },
 };
