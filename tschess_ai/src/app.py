7fdc5c4141107a32715a0891eb1b867201e6bc74
Dominik Kusion <kus22544@spengergasse.at>
2023-05-26 11:22:54 +0200

    AI sogutwiefertig

--- tschess_ai/src/app.py
+++ tschess_ai/src/app.py
@@ -8,25 +8,20 @@ from urllib.parse import *
 
 app = Flask(__name__)
 
-@app.route('/read/<fen>')
-async def ai_move(fen):
-    fen = request.view_args['fen']
-    decoded_fen = unquote(fen)
-    fen = decoded_fen.replace('_', '/')
-    fen = fen.replace('=', ' ')
+@app.route('/read', methods=['POST'])
+async def ai_move():
+    fen = request.json.get('fen')
     _, engine = await chess.engine.popen_uci(['python', 'sunfish/sunfish/sunfish.py'])
     board = chess.Board(fen)
-    result = await engine.play(board, chess.engine.Limit(time=0.1))
+    result = await engine.play(board, chess.engine.Limit(time=5.0))
     board.push(result.move)
     await engine.quit()
     return jsonify({"fen": board.fen()})
 
 
-@app.route('/encode')
-def encode():
-    fen = input("Enter the FEN string: ")
-    converted_fen = fen.replace('/', '_')
-    converted_fen = converted_fen.replace(' ', '=')
-    return jsonify({"fen": converted_fen})
+
+
+
+
 
 
