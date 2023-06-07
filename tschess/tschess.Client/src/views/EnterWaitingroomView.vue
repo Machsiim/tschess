7354c4764a51e7127f77092afcb30bc34ec2e849
Grosserzbischof <eric.a.fischer@icloud.com>
2023-06-07 13:01:52 +0200

    fERTIGES FRONTEND

--- tschess/tschess.Client/src/views/EnterWaitingroomView.vue
+++ tschess/tschess.Client/src/views/EnterWaitingroomView.vue
@@ -1,278 +1,314 @@
 <script setup>
-import axios from 'axios';
-import signalRService from '../services/SignalRService.js';
+import axios from "axios";
+import signalRService from "../services/SignalRService.js";
 </script>
 
-
 <template>
-    <main>
-        <!DOCTYPE html>
-        <html>
+  <main>
+    <!DOCTYPE html>
+    <html>
+      <section class="test">
+        <div class="IchWillDasNicht" v-if="!joinedQueue">
+          <button v-on:click="connect()" class="custom-button">
+            Join Queue
+          </button>
+        </div>
 
+        <br />
 
-        <section class="test">
-            <div class="IchWillDasNicht" v-if="!joinedQueue">
-                <button v-on:click="connect();">Join Queue</button>
+        <div v-if="joinedQueue">
+          <ul>
+            <h2>Connected Users:</h2>
+            <div v-for="user in connectedUsers" v-bind:key="user">
+              <ul v-if="user != this.$store.state.infos.username">
+                {{
+                  user
+                }}
+                <button v-on:click="challenge(user)" class="custom-button">
+                  Challenge
+                </button>
+              </ul>
             </div>
-
-            <br>
-
-            <div v-if="joinedQueue">
-                <ul>
-                    Connected Users:
-                    <div v-for="user in connectedUsers" v-bind:key="user">
-                        <ul v-if="user != this.$store.state.infos.username">{{ user }} <button
-                                v-on:click="challenge(user)">Challenge</button></ul>
-                    </div>
-                </ul>
-                <div>
-                    Incoming Challenges:
-                </div>
-                <div v-if="activeChallenges != undefined">
-                    <div v-for="challenge in activeChallenges" v-bind:key="challenge">
-                        <ul>{{ challenge }}
-                            <button v-on:click="processChallenge('accepted', challenge)">Accept</button>
-                            <button v-on:click="processChallenge('declined', challenge)">Decline</button>
-                        </ul>
-                    </div>
-                </div>
-
+          </ul>
+          <div><h2>Incoming Challenges:</h2></div>
+          <div v-if="activeChallenges != undefined">
+            <div v-for="challenge in activeChallenges" v-bind:key="challenge">
+              <ul>
+                {{
+                  challenge
+                }}
+                <button
+                  v-on:click="processChallenge('accepted', challenge)"
+                  class="custom-button"
+                >
+                  Accept
+                </button>
+                <button
+                  v-on:click="processChallenge('declined', challenge)"
+                  class="custom-button"
+                >
+                  Decline
+                </button>
+              </ul>
             </div>
-
-        </section>
-
-        </html>
-    </main>
+          </div>
+        </div>
+      </section>
+    </html>
+  </main>
 </template>
-
-
-
 
 <script>
 export default {
-    data() {
-        return {
-            connectedUsers: [],
-            joinedQueue: false,
-            activeChallenges: [],
-        };
+  data() {
+    return {
+      connectedUsers: [],
+      joinedQueue: false,
+      activeChallenges: [],
+    };
+  },
+
+  mounted() {},
+
+  unmounted() {
+    signalRService.leaveWaitingroom();
+  },
+
+  computed: {},
+  methods: {
+    async connect() {
+      await signalRService.connectWithToken(this.$store.state.infos.token);
+      signalRService.subscribeEvent("SetWaitingroomState", this.addUser);
+      signalRService.subscribeEvent("GetChallenges", this.printChallenges);
+      signalRService.enterWaitingroom();
+      signalRService.subscribeEvent("GameStarted", this.pushRouter);
+      this.joinedQueue = true;
+    },
+    async enterWaitingroom() {
+      signalRService.enterWaitingroom();
     },
 
-    mounted() {
-
+    addUser(names) {
+      this.connectedUsers = names;
     },
 
-    unmounted() {
-        signalRService.leaveWaitingroom();
+    challenge(username) {
+      signalRService.subscribeEvent("GameStarted", this.pushRouter);
+      signalRService.challenge(username);
     },
 
-    computed: {
-
+    printChallenges(challenges) {
+      if (challenges[1] == this.$store.state.infos.username) {
+        this.activeChallenges.push(challenges[0]);
+      }
     },
-    methods: {
-        async connect() {
-            await signalRService.connectWithToken(this.$store.state.infos.token);
-            signalRService.subscribeEvent("SetWaitingroomState", this.addUser);
-            signalRService.subscribeEvent("GetChallenges", this.printChallenges);
-            signalRService.enterWaitingroom();
-            signalRService.subscribeEvent("GameStarted", this.pushRouter);
-            this.joinedQueue = true;
-        },
-        async enterWaitingroom() {
-            signalRService.enterWaitingroom();
-        },
 
-        addUser(names) {
-            this.connectedUsers = names;
-        },
-
-        challenge(username) {
-            signalRService.subscribeEvent("GameStarted", this.pushRouter);
-            signalRService.challenge(username);
-        },
-
-        printChallenges(challenges) {
-            if (challenges[1] == this.$store.state.infos.username) {
-                this.activeChallenges.push(challenges[0]);
-            }
-        },
-
-        processChallenge(state, challenge) {
-            if (state == "accepted") {
-                const index = this.activeChallenges.indexOf(challenge);
-                if (index > -1) {
-                    this.activeChallenges.splice(index, 1);
-                }
-                signalRService.startGame(challenge)
-            }
-            else {
-                const index = this.activeChallenges.indexOf(challenge);
-                if (index > -1) {
-                    this.activeChallenges.splice(index, 1);
-                }
-            }
-        },
-
-        async pushRouter(gameId) {
-            this.$store.commit("joinGame", gameId);
-            this.leaveWaitingroom();
-            await this.$router.push("/game/");
-        },
-
-        leaveWaitingroom() {
-            signalRService.leaveWaitingroom();
+    processChallenge(state, challenge) {
+      if (state == "accepted") {
+        const index = this.activeChallenges.indexOf(challenge);
+        if (index > -1) {
+          this.activeChallenges.splice(index, 1);
         }
-
+        signalRService.startGame(challenge);
+      } else {
+        const index = this.activeChallenges.indexOf(challenge);
+        if (index > -1) {
+          this.activeChallenges.splice(index, 1);
+        }
+      }
     },
+
+    async pushRouter(gameId) {
+      this.$store.commit("joinGame", gameId);
+      this.leaveWaitingroom();
+      await this.$router.push("/game/");
+    },
+
+    leaveWaitingroom() {
+      signalRService.leaveWaitingroom();
+    },
+  },
 };
 </script>
 
-
 <style>
+.custom-button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #8a2be2; /* Purple color */
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+.custom-button + .custom-button {
+  margin-left: 10px;
+}
+.custom-button:hover {
+  background-color: #6a1c9a; /* Darker shade of purple on hover */
+}
 header {
-    color: white;
+  color: white;
 }
 
 .test {
-    text-align: center;
+  text-align: center;
 }
 
 body {
-    margin: 0;
-    padding: 0;
-    font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
 }
 
 section {
-    float: left;
-    width: 70%;
-    padding: 20px;
-    color: white;
+  /* Remove the float and width properties */
+  float: none;
+  width: auto;
+
+  /* Add centering properties */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 20px;
+  color: white;
+  margin-top: -90px !important;
 }
 
 aside {
-    float: left;
-    width: 30%;
-    padding: 20px;
+  float: left;
+  width: 30%;
+  padding: 20px;
 }
 
 footer {
-    background-color: #222;
-    color: #fff;
-    padding: 20px;
-    text-align: center;
-    font-size: 0.8em;
-    clear: both;
+  background-color: #222;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+  font-size: 0.8em;
+  clear: both;
 }
 
 .carousel {
-    position: relative;
-    width: 70%;
-    height: 500px;
-    overflow: hidden;
-    margin-left: 15%;
-    transition: transform 1s ease-in-out;
+  position: relative;
+  width: 70%;
+  height: 500px;
+  overflow: hidden;
+  margin-left: 15%;
+  transition: transform 1s ease-in-out;
 }
 
 .carousel img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 
 .carousel button {
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    font-size: 2rem;
-    background-color: transparent;
-    border: none;
-    color: white;
-    cursor: pointer;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 2rem;
+  background-color: transparent;
+  border: none;
+  color: white;
+  cursor: pointer;
 }
 
 .carousel button:first-child {
-    left: 0;
+  left: 0;
 }
 
 .carousel button:last-child {
-    right: 0;
+  right: 0;
 }
 
 .container {
-    display: flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
 }
 
 .book {
-    position: relative;
-    border-radius: 10px;
-    width: 220px;
-    height: 300px;
-    background-color: whitesmoke;
-    -webkit-box-shadow: 1px 1px 12px #000;
-    box-shadow: 1px 1px 12px #000;
-    -webkit-transform: preserve-3d;
-    -ms-transform: preserve-3d;
-    transform: preserve-3d;
-    -webkit-perspective: 2000px;
-    perspective: 2000px;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    color: #000;
+  position: relative;
+  border-radius: 10px;
+  width: 220px;
+  height: 300px;
+  background-color: whitesmoke;
+  -webkit-box-shadow: 1px 1px 12px #000;
+  box-shadow: 1px 1px 12px #000;
+  -webkit-transform: preserve-3d;
+  -ms-transform: preserve-3d;
+  transform: preserve-3d;
+  -webkit-perspective: 2000px;
+  perspective: 2000px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  color: #000;
 }
 
 .cover {
-    top: 0;
-    position: absolute;
-    background-color: lightgray;
-    width: 100%;
-    height: 100%;
-    border-radius: 10px;
-    cursor: pointer;
-    -webkit-transition: all 0.5s;
-    transition: all 0.5s;
-    -webkit-transform-origin: 0;
-    -ms-transform-origin: 0;
-    transform-origin: 0;
-    -webkit-box-shadow: 1px 1px 12px #000;
-    box-shadow: 1px 1px 12px #000;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
+  top: 0;
+  position: absolute;
+  background-color: lightgray;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  cursor: pointer;
+  -webkit-transition: all 0.5s;
+  transition: all 0.5s;
+  -webkit-transform-origin: 0;
+  -ms-transform-origin: 0;
+  transform-origin: 0;
+  -webkit-box-shadow: 1px 1px 12px #000;
+  box-shadow: 1px 1px 12px #000;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
 }
 
 .book:hover .cover {
-    -webkit-transition: all 0.5s;
-    transition: all 0.5s;
-    -webkit-transform: rotatey(-80deg);
-    -ms-transform: rotatey(-80deg);
-    transform: rotatey(-80deg);
+  -webkit-transition: all 0.5s;
+  transition: all 0.5s;
+  -webkit-transform: rotatey(-80deg);
+  -ms-transform: rotatey(-80deg);
+  transform: rotatey(-80deg);
+}
+.test {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.test > * {
+  flex: 1;
+  text-align: center;
 }
 
 p {
-    font-size: 15px;
-    font-weight: bolder;
+  font-size: 15px;
+  font-weight: bolder;
 }
 
 img {
-    height: 300px;
+  height: 300px;
 }
-
 </style>
